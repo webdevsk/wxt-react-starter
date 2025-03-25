53d6d0064a7fb41c1504a4d89cdbb629e9be953a
webdevsk <72684302+webdevsk@users.noreply.github.com>
2025-03-25 15:00:24 +0600

    Migrated to eslint.config.ts

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,6 +3,7 @@ import globals from "globals"
 import js from "@eslint/js"
 import tseslint from "typescript-eslint"
 import pluginReact from "eslint-plugin-react"
+// @ts-expect-error wxt doesn't provide types for it yet
 import autoImports from "./.wxt/eslint-auto-imports.mjs"
 // sets up both eslint plugin and eslint config in one go
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended"
@@ -17,7 +18,8 @@ export default defineConfig([
   },
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
   tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  // pluginReact.configs.flat.recommended doesn't have predefined settings property. So no need to spread it over here
+  { ...pluginReact.configs.flat.recommended, settings: { react: { version: "19" } } },
   pluginReact.configs.flat["jsx-runtime"],
   eslintPluginPrettierRecommended,
 ])
